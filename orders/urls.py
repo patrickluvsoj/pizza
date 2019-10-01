460ad3bb5a424edd922e50df753b7596f834bfa2
Patrick Suzuki <hpatricksuzuki@gmail.com>
2019-09-30 18:41:06 -0700

    added forms for each menu category and enabled URl history

--- orders/urls.py
+++ orders/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("menu", views.menu, name="menu"),
+    path("menu/", views.menu, name="menu"),
+    path("menu/<str:category>/", views.submenu, name="submenu"),
     path("cart", views.cart, name="cart"),
     path("register", views.register, name="register")
 ]
--- orders/views.py
+++ orders/views.py
@@ -6,10 +6,52 @@ def index(request):
     return HttpResponse("Login page")
 
 def menu(request):
-    return HttpResponse("Menu page")
+    return render(request, 'menu.html')
+
+def submenu(request, category):
+    return HttpResponse(category)
 
 def cart(request):
     return HttpResponse("Cart page")
 
 def register(request):
     return HttpResponse("Sign-up page")
+
+
+# TODO
+#1 SPA
+#   hello.html
+#   download sample projects from course
+#   make simple SPA
+#   enable HTML history
+#   dice3.html use handle bars to  enable actual forms
+
+
+#2 Auth
+#   setup login and registratgion page
+#   setup authenticaiton using Django
+
+
+#3 DB  
+#   Setup DB model
+#   Hook-up form w/ DB model
+#   Show raw results in a page
+
+
+
+#4 Carts
+#   Create cart page
+#   Quesry products in cart
+#   Enable subnit order and store order history
+
+#5 History
+#   Setup order histopry  page
+#   Create order details from  history (re-use cart page?)
+
+#6 Update cart
+#   Enable update/delete cart
+#   Add animation for deleting products
+
+#7 Additional features
+#   Add SMS
+#   Enable Stripe
